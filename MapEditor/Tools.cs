26d1395ce32edcbe01afe420e40cd3b3492d22ad
Ro5hi <slen.detablan@gmail.com>
2021-08-04 15:37:05 -0600

    added assets, working on map tools

---
+++ MapEditor/Tools.cs
@@ -0,0 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
