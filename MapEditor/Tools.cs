c575532800cdf63591a35ec0dafd22706a08b763
Ro5hi <slen.detablan@gmail.com>
2021-08-04 15:44:36 -0600

    added namespace and public classes for tool manager

--- MapEditor/Tools.cs
+++ MapEditor/Tools.cs
@@ -5,3 +5,19 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+
+namespace Tools
+{
+    public class Element
+    {
+        public int ID;
+        public Image image;
+
+        <param name="id">ID</param>
+        <param name="img">Image</param>
+        
+        public TileSetElement(int id, Image img)
+            ID = id;
+            Image = img; 
+    }
+}
